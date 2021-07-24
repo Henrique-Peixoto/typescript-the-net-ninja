4ce2dc691329721411735b8d555dfd811a2a9274
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-07-24 17:30:53 -0300

    Lesson code

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,44 @@
 "use strict";
-console.log("Howdy?");
+// Implicity function type
+var greet = function () {
+    console.log('Hello, World!');
+};
+// greet = 'Hello, World!';
+// Explicity function type
+var hello;
+hello = function () {
+    console.log('Hello!');
+};
+// Function using parameters with type
+var add;
+add = function (a, b) {
+    console.log(a + b);
+};
+add(5, 10);
+// Parameter with question mark
+var add2 = function (a, b, c) {
+    console.log(a + b);
+    console.log(c);
+};
+add2(5, 10);
+add2(5, 10, 25);
+// Parameter with default value
+var add3 = function (a, b, c) {
+    if (c === void 0) { c = 'Hi'; }
+    console.log(a + b);
+    console.log(c);
+};
+add3(5, 10);
+add3(5, 10, -1);
+// Function return
+// Returning void
+var add4 = function (a, b, c) {
+    console.log(a + b);
+    console.log(c);
+};
+// Returning a number
+var add5 = function (a, b) {
+    return a + b;
+};
+var sumResult = add5(20, 1);
+console.log(sumResult);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,57 @@
-console.log("Howdy?");
+// Implicity function type
+let greet = () => {
+  console.log('Hello, World!');
+}
+
+// greet = 'Hello, World!';
+
+// Explicity function type
+let hello: Function;
+
+hello = () => {
+  console.log('Hello!');
+}
+
+// Function using parameters with type
+let add: Function;
+
+add = (a: number, b: number) => {
+  console.log(a + b);
+}
+
+add(5,10);
+
+// Parameter with question mark
+const add2 = (a: number, b: number, c?: number | string) => {
+  console.log(a + b);
+  console.log(c);
+}
+
+add2(5,10);
+add2(5,10,25);
+
+// Parameter with default value
+const add3 = (a: number, b: number, c: number | string = 'Hi') => {
+  console.log(a + b);
+  console.log(c);
+}
+
+add3(5,10);
+add3(5,10,-1);
+
+// Function return
+
+// Returning void
+const add4 = (a: number, b: number, c?: number | string): void => {
+  console.log(a + b);
+  console.log(c);
+}
+
+
+// Returning a number
+const add5 = (a: number, b: number): number => {
+  return a + b;
+}
+
+let sumResult = add5(20,1);
+console.log(sumResult);
