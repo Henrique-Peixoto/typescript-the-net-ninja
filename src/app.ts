b3ead395219c93d1d59cf2b2394adf89e8eed157
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-07-27 16:58:17 -0300

    Lesson code

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import { Invoice } from './classes/Invoice.js';
+
+const invOne = new Invoice('Tolkien','a new The Lord of the Rings book', 500);
+const invTwo = new Invoice('Martin','a new The Chronicles of Ice and Fire book', 300);
+
+console.log(invOne, invTwo);
